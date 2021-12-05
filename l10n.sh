3982700d57e3b1a87e2398dcfbcbba16377e816c
Kitsune Solar <kitsune.solar@gmail.com>
2021-12-05 16:34:30 +0300

    2021-12-05 13:34:30

--- l10n.sh
+++ l10n.sh
@@ -1,66 +1,118 @@
 #!/bin/bash
 
-REPO="${1}"
-USER="${2}"
-EMAIL="${3}"
-TOKEN="${4}"
+init() {
+  REPO="${1}"
+  USER="${2}"
+  EMAIL="${3}"
+  TOKEN="${4}"
 
-map="_exts.txt"
-header="Authorization: token ${TOKEN}"
+  map="_exts.txt"
+  header="Authorization: token ${TOKEN}"
+  path_repo="/root/git/l10n"
 
-curl="$( command -v curl )"
-date="$( command -v date )"
-git="$( command -v git )"
-jq="$( command -v jq )"
-sed="$( command -v sed )"
+  curl="$( command -v curl )"
+  date="$( command -v date )"
+  git="$( command -v git )"
+  jq="$( command -v jq )"
+  sed="$( command -v sed )"
 
-${git} config --global user.email "${EMAIL}"
-${git} config --global user.name "${USER}"
-${git} config --global init.defaultBranch 'main'
+  ${git} config --global user.email "${EMAIL}"
+  ${git} config --global user.name "${USER}"
+  ${git} config --global init.defaultBranch 'main'
 
-REPO_AUTH="https://${USER}:${TOKEN}@${REPO#https://}"
+  git_clone     \
+    && get_l10n \
+    && git_push
+}
 
-${git} clone "${REPO_AUTH}" '/root/git/l10n' \
-  && cd '/root/git/l10n' || exit 1
-${git} remote add 'l10n' "${REPO_AUTH}"
+# -------------------------------------------------------------------------------------------------------------------- #
+# GIT: CLONE REPOSITORY.
+# -------------------------------------------------------------------------------------------------------------------- #
 
-mapfile -t exts < "${map}"
+git_clone() {
+  REPO_AUTH="https://${USER}:${TOKEN}@${REPO#https://}"
 
+  ${git} clone "${REPO_AUTH}" "${path_repo}" \
+    && _pushd "${path_repo}" || exit 1
+  ${git} remote add 'l10n' "${REPO_AUTH}"
+
+  mapfile -t exts < "${map}"
+  _popd || exit 1
+}
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# L10N: GET LANGUAGE FILES.
+# -------------------------------------------------------------------------------------------------------------------- #
+
+get_l10n() {
+  mkdir -p "${path_repo}/locale" \
+    && _pushd "${path_repo}/locale" || exit 1
+
+  for ext in "${exts[@]}"; do
+    echo "--- Open: '${ext}'"
+
+    url_api=$( _get_api "https://api.github.com/repos/${ext}/contents/resources/locale/en.yml" )
+    url_api_res="${?}"
+
+    if [[ ${url_api_res} != "0" ]]; then
+      url_api=$( _get_api "https://api.github.com/repos/${ext}/contents/locale/en.yml" )
+    fi
+
+    url_download=$( echo "${url_api}" | ${jq} -r '.download_url' )
+    name=$( _get_api "${url_download}" | ${sed} -n 1p | ${sed} "s/://g" )
+    _get_file "${url_download}" "${name}.yml"
+  done
+
+  _popd || exit 1
+}
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# GIT: PUSH REPOSITORY.
+# -------------------------------------------------------------------------------------------------------------------- #
+
+git_push() {
+  _pushd "${path_repo}" || exit 1
+
+  ts="$( _timestamp )"
+
+  ${git} add . \
+    && ${git} commit -a -m "L10N: ${ts}" \
+    && ${git} push 'l10n'
+
+  _popd || exit 1
+}
+
+# -------------------------------------------------------------------------------------------------------------------- #
+# ------------------------------------------------< COMMON FUNCTIONS >------------------------------------------------ #
+# -------------------------------------------------------------------------------------------------------------------- #
+
+# Pushd.
+_pushd() {
+  command pushd "$@" > /dev/null || exit 1
+}
+
+# Popd.
+_popd() {
+  command popd > /dev/null || exit 1
+}
+
+# Timestamp.
 _timestamp() {
   ${date} -u '+%Y-%m-%d %T'
 }
 
-_getAPI() {
+# Get API.
+_get_api() {
   ${curl} -sf -X GET -H "${header}" "${1}"
 }
 
-_getFile() {
+# Get files.
+_get_file() {
   ${curl} -sf -X GET -H "${header}" "${1}" -o "${2}"
 }
 
-getL10N() {
-  for ext in "${exts[@]}"; do
-    echo "--- Open: '${ext}'"
+# -------------------------------------------------------------------------------------------------------------------- #
+# -------------------------------------------------< INIT FUNCTIONS >------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------------- #
 
-    url_api=$( _getAPI "https://api.github.com/repos/${ext}/contents/resources/locale/en.yml" )
-    url_api_res="${?}"
-
-    if [[ ${url_api_res} != "0" ]]; then
-      url_api=$( _getAPI "https://api.github.com/repos/${ext}/contents/locale/en.yml" )
-    fi
-
-    url_dwn=$( echo "${url_api}" | ${jq} -r '.download_url' )
-    name=$( _getAPI "${url_dwn}" | ${sed} -n 1p | ${sed} "s/://g" )
-    _getFile "${url_dwn}" "${name}.yml"
-  done
-}
-
-getL10N
-
-ts="$( _timestamp )"
-
-${git} add . \
-  && ${git} commit -a -m "L10N: ${ts}" \
-  && ${git} push 'l10n'
-
-exit 0
+init "$@"; exit 0
